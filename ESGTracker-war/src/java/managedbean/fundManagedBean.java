5c3aa120096d30fff711dd64d34d833e1e36795e
XindiTian <e0425890@u.nus.edu>
2022-11-05 17:55:38 +0800

    configuration files and implement jsf

---
+++ ESGTracker-war/src/java/managedbean/fundManagedBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbean;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author xindi
+ */
+@Named(value = "fundManagedBean")
+@RequestScoped
+public class fundManagedBean {
+
+    /**
+     * Creates a new instance of fundManagedBean
+     */
+    public fundManagedBean() {
+    }
+    
+}
