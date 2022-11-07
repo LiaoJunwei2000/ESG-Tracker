1dd95f9b1795414421c342b97d23bb60e17af0c1
XindiTian <e0425890@u.nus.edu>
2022-11-08 05:34:03 +0800

    Delete Fund method

--- ESGTracker-war/src/java/managedbean/FundManagedBean.java
+++ ESGTracker-war/src/java/managedbean/FundManagedBean.java
@@ -25,10 +25,10 @@ import session.FundSessionLocal;
 @Named(value = "fundManagedBean")
 @ViewScoped
 public class FundManagedBean implements Serializable {
-    
+
     @EJB
     private FundSessionLocal fundSessionLocal;
-    
+
     private String fundIdentifier;
     private String fundName;
     private int fyear;
@@ -36,19 +36,19 @@ public class FundManagedBean implements Serializable {
     private int gresb;
     private boolean unrpi;
     private boolean tfcd;
-    
+
     private List<Fund> funds;
     private Fund selectedFund;
     private Long fundId;
-    
+
     public FundManagedBean() {
     }
-    
+
     @PostConstruct
     public void init() {
         setFunds(fundSessionLocal.searchFunds(null));
     }
-    
+
     public void addFund(ActionEvent evt) {
         Fund f = new Fund();
         f.setName(fundName);
@@ -58,13 +58,13 @@ public class FundManagedBean implements Serializable {
         f.setGRESBRating(gresb);
         f.setInTFCD(tfcd);
         f.setInUNRPI(unrpi);
-        
+
         fundSessionLocal.createFund(f);
     }
-    
+
     public void loadSelectedFund() {
         FacesContext context = FacesContext.getCurrentInstance();
-        
+
         try {
             this.selectedFund
                     = fundSessionLocal.getFund(fundId);
@@ -79,7 +79,7 @@ public class FundManagedBean implements Serializable {
             context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Unable to load fund"));
         }
     }
-    
+
     public void updateFund(ActionEvent evt) {
         FacesContext context = FacesContext.getCurrentInstance();
         selectedFund.setFundIdentifier(fundIdentifier);
@@ -89,7 +89,7 @@ public class FundManagedBean implements Serializable {
         selectedFund.setFquarter(fquarter);
         selectedFund.setInTFCD(tfcd);
         selectedFund.setInUNRPI(unrpi);
-        
+
         try {
             fundSessionLocal.updateFund(selectedFund);
         } catch (Exception e) {
@@ -101,7 +101,7 @@ public class FundManagedBean implements Serializable {
         init();
         context.addMessage(null, new FacesMessage("Success", "Successfully updated fund entry"));
     }
-    
+
     public void deleteCustomer() {
         FacesContext context = FacesContext.getCurrentInstance();
         Map<String, String> params = context.getExternalContext()
@@ -118,93 +118,110 @@ public class FundManagedBean implements Serializable {
         context.addMessage(null, new FacesMessage("Success", "Successfully deleted fund entry"));
         init();
     }
-    
+
+    public void deleteFund() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        Map<String, String> params = context.getExternalContext()
+                .getRequestParameterMap();
+        String fIdStr = params.get("fundId");
+        Long fId = Long.parseLong(fIdStr);
+        try {
+            fundSessionLocal.deleteFund(fId);
+        } catch (Exception e) {
+//show with an error icon
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Unable to delete fund"));
+            return;
+        }
+        context.addMessage(null, new FacesMessage("Success", "Successfully deleted fund entry"));
+        init();
+    }
+
     public FundSessionLocal getFundSessionLocal() {
         return fundSessionLocal;
     }
-    
+
     public void setFundSessionLocal(FundSessionLocal fundSessionLocal) {
         this.fundSessionLocal = fundSessionLocal;
     }
-    
+
     public String getFundIdentifier() {
         return fundIdentifier;
     }
-    
+
     public void setFundIdentifier(String fundIdentifier) {
         this.fundIdentifier = fundIdentifier;
     }
-    
+
     public String getFundName() {
         return fundName;
     }
-    
+
     public void setFundName(String fundName) {
         this.fundName = fundName;
     }
-    
+
     public int getFyear() {
         return fyear;
     }
-    
+
     public void setFyear(int fyear) {
         this.fyear = fyear;
     }
-    
+
     public int getFquarter() {
         return fquarter;
     }
-    
+
     public void setFquarter(int fquarter) {
         this.fquarter = fquarter;
     }
-    
+
     public int getGresb() {
         return gresb;
     }
-    
+
     public void setGresb(int gresb) {
         this.gresb = gresb;
     }
-    
+
     public boolean isUnrpi() {
         return unrpi;
     }
-    
+
     public void setUnrpi(boolean unrpi) {
         this.unrpi = unrpi;
     }
-    
+
     public boolean isTfcd() {
         return tfcd;
     }
-    
+
     public void setTfcd(boolean tfcd) {
         this.tfcd = tfcd;
     }
-    
+
     public List<Fund> getFunds() {
         return funds;
     }
-    
+
     public void setFunds(List<Fund> funds) {
         this.funds = funds;
     }
-    
+
     public Fund getSelectedFund() {
         return selectedFund;
     }
-    
+
     public void setSelectedFund(Fund selectedFund) {
         this.selectedFund = selectedFund;
     }
-    
+
     public Long getFundId() {
         return fundId;
     }
-    
+
     public void setFundId(Long fundId) {
         this.fundId = fundId;
     }
-    
+
 }
